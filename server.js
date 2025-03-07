61a0770a5ad5623926c7d0e8f5a1f0fcfe26cb54
Subiksha <mvsubiksha@gmail.com>
2025-03-07 07:55:56 +0530

    updated server and db for render deployment

--- server.js
+++ server.js
@@ -14,5 +14,10 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 8080; // Use Render's PORT
+
+app.get("/", (req, res) => {
+  res.send("API is running on Render!");
+});
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
