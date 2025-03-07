08eaf6c2e57ca2a6e910658cb440df197325cc75
Subiksha <mvsubiksha@gmail.com>
2025-03-07 11:25:48 +0530

    updated user

--- models/user.js
+++ models/user.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    id: { type: Number, required: true },
-    phoneNumber: { type: String, required: true, unique: true }, // UNIQUE FIELD
-    createdAt: { type: Date, default: Date.now },
-    referralCode: { type: String },
-    profilePhoto: { type: String }
-  });
-  
-  const User = mongoose.model("User", userSchema);
-  
-  module.exports = User;
+  id: { type: Number, required: true }, // REQUIRED FIELD
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  phoneNumber: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now },
+  referralCode: { type: String },
+  profilePhoto: { type: String }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
